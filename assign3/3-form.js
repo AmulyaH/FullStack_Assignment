a15425742f098c2b64ea8a5d992dfacb9caafd0a
amulya <amulya2@pdx.edu>
2020-05-05 16:23:30 -0700

    3-form submission

--- assign3/3-form.js
+++ assign3/3-form.js
@@ -1,1 +1,11 @@
-// Enter your code here
+
+function saveForm(){
+    var name = document.getElementById("txtname").value;
+    var email = document.getElementById("txtemail").value;
+    var message = document.getElementById("txtmessage").value;
+    var newsLetter = document.getElementById("chk").value;
+    console.log(" name: " + name + "\n email: " + email + "\n comments: " + message + "\n newsletter: " + newsLetter);
+}
+
+
+
