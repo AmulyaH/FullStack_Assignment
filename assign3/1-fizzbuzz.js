45f5975e7e2d170b2d4f33a4b15b29b394ab9562
amulya <amulya2@pdx.edu>
2020-05-05 11:38:50 -0700

    cheching in fizzbuzz

--- assign3/1-fizzbuzz.js
+++ assign3/1-fizzbuzz.js
@@ -1,1 +1,20 @@
-// Enter your code here
+var i;
+var number = "";
+for (i = 1; i <= 100 ; i++) {
+    if(i % 5 == 0 && i % 3 == 0)
+    {
+        number += 'fizzbuzz' + "<br>"
+    }
+    else if(i % 5 == 0 && i % 3 != 0)
+    {
+        number += 'buzz' + "<br>";
+    }
+    else if(i%3 == 0)
+    {
+        number += 'fizz' + "<br>";
+    }
+    else{
+        number += i + "<br>";
+    }
+  }
+  document.getElementById("results").innerHTML = number;
