c515aacb318d49d1cdde602fe8723a338014a42d
amulya <amulya2@pdx.edu>
2020-05-05 21:30:54 -0700

    6- async api request

--- assign3/5-api.js
+++ assign3/5-api.js
@@ -4,24 +4,25 @@ let xhr = new XMLHttpRequest();
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
 xhr.send();
+
 xhr.onload = () => {
-if (xhr.status === 200) {
-let data = xhr.response;
-console.log(data);
-    for(var i=0; i< data.length; i++)
+    if (xhr.status === 200) 
     {
-        var entry = data[i];
-        var ol = document.getElementById("results");
-        var li = document.createElement("li");
-        li.setAttribute('id',entry.name);
-        var text =  entry.name + " : " + entry.population;
-        li.appendChild(document.createTextNode(text));
-        ol.appendChild(li);
+        let data = xhr.response;
+        data.forEach(function(entry) 
+        {
+            var ol = document.getElementById("results");
+            var li = document.createElement("li");
+            li.setAttribute('id',entry.name);
+            var text =  entry.name + " : " + entry.population;
+            li.appendChild(document.createTextNode(text));
+            ol.appendChild(li);
+
+        })
     }
-}
-else {
-console.log(`Status Code: ${xhr.status} - ${xhr.statusText}`);
-}
+    else 
+    {
+        console.log(`Status Code: ${xhr.status} - ${xhr.statusText}`);
+    }
 };
 
-// Enter your code here
--- assign3/6-pokemon.js
+++ assign3/6-pokemon.js
@@ -1,3 +1,30 @@
+
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
-// Enter your code here
+let div = document.getElementById("result");
+
+let getDataAsync = async (url) => 
+{
+    try 
+    {
+        let response = await fetch(url);
+        let data = await response.json();
+       
+        data.results.forEach(function(result) {
+            let span = document.createElement("span");
+            let br = document.createElement("br");
+            let name = (result.name).toUpperCase();
+            span.setAttribute('id',name);
+            span.appendChild(document.createTextNode(name));
+            span.appendChild(br);
+            div.appendChild(span);
+        });
+        
+    }
+    catch(error)
+    {
+        console.log('Request failed', error);
+    }
+};
+
+getDataAsync(url);             
