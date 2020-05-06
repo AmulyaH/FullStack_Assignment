5550160b084e8423905417feaa8bf63d800b59f4
amulya <amulya2@pdx.edu>
2020-05-05 17:05:38 -0700

    4-window resize

--- assign3/4-events.js
+++ assign3/4-events.js
@@ -1,1 +1,8 @@
-// Enter your code here
+window.addEventListener('resize', resizeWindow);
+	resizeWindow();
+    
+function resizeWindow()
+	{
+  		document.querySelector('#width').innerText = document.documentElement.clientWidth;
+		document.querySelector('#height').innerText = document.documentElement.clientHeight;
+	}
