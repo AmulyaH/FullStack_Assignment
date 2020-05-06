de4be283c2bc09887c7ae55be5abfeba1fb26c25
amulya <amulya2@pdx.edu>
2020-05-05 20:03:27 -0700

    get request api call

--- assign3/5-api.js
+++ assign3/5-api.js
@@ -1,3 +1,27 @@
 const url = 'https://restcountries.eu/rest/v2/all';
 
+let xhr = new XMLHttpRequest();
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+xhr.send();
+xhr.onload = () => {
+if (xhr.status === 200) {
+let data = xhr.response;
+console.log(data);
+    for(var i=0; i< data.length; i++)
+    {
+        var entry = data[i];
+        var ol = document.getElementById("results");
+        var li = document.createElement("li");
+        li.setAttribute('id',entry.name);
+        var text =  entry.name + " : " + entry.population;
+        li.appendChild(document.createTextNode(text));
+        ol.appendChild(li);
+    }
+}
+else {
+console.log(`Status Code: ${xhr.status} - ${xhr.statusText}`);
+}
+};
+
 // Enter your code here
