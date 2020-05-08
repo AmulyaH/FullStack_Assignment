0d40d94773db3a418ea2b355aa550938252facd0
AmulyaH <amulya2@pdx.edu>
2020-05-07 21:41:47 -0700

    5 and 6 fetch api fix

--- assign3/5-api.js
+++ assign3/5-api.js
@@ -4,25 +4,28 @@ let xhr = new XMLHttpRequest();
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
 xhr.send();
-
 xhr.onload = () => {
     if (xhr.status === 200) 
     {
         let data = xhr.response;
         data.forEach(function(entry) 
         {
-            var ol = document.getElementById("results");
-            var li = document.createElement("li");
+            let ol = document.getElementById("results");
+            let li = document.createElement("li");
             li.setAttribute('id',entry.name);
-            var text =  entry.name + " : " + entry.population;
+            let population = entry.population;
+            let text =  entry.name + " : " + population;
             li.appendChild(document.createTextNode(text));
             ol.appendChild(li);
-
         })
     }
     else 
     {
         console.log(`Status Code: ${xhr.status} - ${xhr.statusText}`);
     }
-};
+}; 
+
+
+
+
 
--- assign3/6-pokemon.js
+++ assign3/6-pokemon.js
@@ -2,23 +2,39 @@
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
 let div = document.getElementById("result");
+let count = 0;
 
 let getDataAsync = async (url) => 
 {
     try 
     {
+       let run =  true;
+       while(run)
+       {
         let response = await fetch(url);
-        let data = await response.json();
+        
+        let data = await response.json(); 
+        
+        if(data.next)
+        {
+            run = true;
+            url = data.next;
+        }
+        else{
+            run = false;
+        }
        
         data.results.forEach(function(result) {
             let span = document.createElement("span");
             let br = document.createElement("br");
             let name = (result.name).toUpperCase();
+            count ++;
             span.setAttribute('id',name);
-            span.appendChild(document.createTextNode(name));
+            span.appendChild(document.createTextNode( count + ") " + name ));
             span.appendChild(br);
             div.appendChild(span);
         });
+   }
         
     }
     catch(error)
@@ -27,4 +43,4 @@ let getDataAsync = async (url) =>
     }
 };
 
-getDataAsync(url);             
+getDataAsync(url);   
