4ea09156fe59aab700912d7a6cf3f6aa8b915bb6
AmulyaH <amulya2@pdx.edu>
2020-05-15 16:22:52 -0700

    assignment 4 form 1

---
+++ assign4/1-form/1-form.js
@@ -0,0 +1,31 @@
+const port = 8080;
+
+var express = require('express'),
+    app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/',function(req,res){
+    var name = req.body.fname;
+    var email= req.body.email;
+    var comment = req.body.message;
+
+    res.write('Name:'+' '+name+'\n');
+    res.write('Email:'+' '+email+'\n');
+    res.write('Comment:'+' '+comment);
+    
+    res.send();
+});
+
+app.listen(port);
